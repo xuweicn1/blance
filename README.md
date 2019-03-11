3ebe86347e44c046e12c489bf63373b7dc4a145c
xuweicn1 <821382128@qq.com>
2019-03-11 11:23:51 +0800

    20190311_v0.1

--- README.md
+++ README.md
@@ -4,16 +4,7 @@
 
 实现功能：
 
-1. 取物体重量精确值 ±2g
+1. 取物体重量精确值 ±3g
 2. 重物偏心位置
 
 
-
-程序：
-
-
-- hx711：驱动程序
-- ratio: 称重系数
-- calculate_weight：只称重
-- weight-to-csv.py：导出数据到CSV
-- coordinate.py ：计算重心偏移
--- calculate-weight.py
+++ calculate-weight.py
@@ -6,19 +6,16 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-
-DT1 = 17
-SCK1 = 18
-
-DT2 = 22
-SCK2 = 23
-
-DT3 = 26
-SCK3 = 20
+DT1, SCK1 = 17, 18
+DT2, SCK2 = 22, 23
+DT3, SCK3 = 26, 20
 
 DIF = 250  # 波动系数
-ROT = 300.37  # 称重系数
 
+# 称重系数
+ROT = [298.97833333333955, 295.845, 298.45, 302.64]
+
+R = 187.5   # 圆盘半径
 
 def zero_fun():
     """初始值"""
@@ -28,41 +25,62 @@ def zero_fun():
     return (zero1 + zero2 + zero3)/3, zero1, zero2, zero3,
 
 
-def calculate_difference():
-    """计算读数差值"""
+def calculate_ratio(W=50):
+    """计算称重系数"""
+    print('请放{}g砝码...'.format(W))
+    time.sleep(5)
     cont = zero_fun()
-    d0 = cont[0] - initial[0]
-    d1 = cont[1] - initial[1]
-    d2 = cont[2] - initial[2]
-    d3 = cont[3] - initial[3]
-    
-    # 过滤波动值
-    if d0 < DIF:
-        d0 = 0
-    if d1 < DIF:
-        d1 = 0
-    if d2 < DIF:
-        d2 = 0
-    if d3 < DIF:
-        d3 = 0
-
-    return d0, d1, d2, d3
+    d = list(map(lambda x, y: x - y, cont, initial))
+    r = [i/W for i in d]
+    return r
 
 
 def calculate_weight():
-    """计算重量"""
-    d = calculate_difference()
-    w = [i/ROT for i in d]
+    """计算每个秤的重量"""
+    cont = zero_fun()
+    d = list(map(lambda x, y: x - y, cont, initial))    # 做差
+    d = list(map(lambda x: x if x > DIF else 0, d))  # 过滤波动
+    w = list(map(lambda x, y: x / y, d, ROT))   # 称重
     return w
 
+def weigth_to_csv(file,lst,lenth):
+    """将列表写入文件"""
+    with open(file, 'a') as csvfile:
+        for i, v in enumerate(lst):
+            csvfile.write(str(v)+',')
+        csvfile.write(str(lenth)+'\n')
+        
+def coordinate(m,M,R):
+    """
+    y = -((2*R)/(3*M))x + 1.667R 线性关系式
+    
+    x：子秤重量
+    y：距离子秤距离
+    """
+    return -((2*R)/(3*M))*m + 1.667*R
 
 initial = zero_fun()
 
+# 初始坐标
+position = [0,0,0]
+
 if __name__ == '__main__':
 
-    while True:
-        time.sleep(2)
-        w = calculate_weight()
-        print('实 重：{:.2f}g;\n秤--1：{:.2f}g;\n秤--2：{:.2f}g;\n秤--3：{:.2f}g;'.format(
-            w[0], w[1], w[2], w[3]))
-        print('*'*20)
+    r = calculate_ratio(871)  # 计算重量系数
+    print(r)
+
+    # while True:
+        # time.sleep(2)
+        # w = calculate_weight()    
+        # weigth_to_csv('0310.csv',w,6.28318530717959)  #导出数据
+        
+        # if w[0] < 3:
+            # position = [0,0,0]
+        # else:
+            # position[0] = coordinate(w[1],w[0],R)
+            # position[1] = coordinate(w[2],w[0],R)
+            # position[2] = coordinate(w[3],w[0],R)
+        # print('实 重：{:.2f}g;\n秤--1：{:.2f}g;\n秤--2：{:.2f}g;\n秤--3：{:.2f}g;'.format(
+            # w[0], w[1], w[2], w[3]))
+        # print(position)
+        # print('*'*20)
